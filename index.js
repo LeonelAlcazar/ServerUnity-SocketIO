adfba4a78272373fb1d984bcfcd885d896ff1d9d
Leonel A. Alcazar <leonelagustinalcazar@gmail.com>
2019-01-06 20:03:46 -0300

    Vehicles!

--- index.js
+++ index.js
@@ -4,6 +4,20 @@ console.log('Server started');
 
 var Players = [];
 
+var Cars = [
+	{
+		"id":"0",
+		"driver":"none",
+		"inUse":"false",
+		"x":"4,00",
+		"y":"0,5",
+		"z":"0",
+		"rx":"0",
+		"ry":"0",
+		"rz":"0",
+	}
+];
+
 
 
 io.on('connection', function(socket){
@@ -21,6 +35,12 @@ io.on('connection', function(socket){
 		socket.emit('spawn', {id: playerId});
 
 	});
+	Cars.forEach(function(car){
+		if(car.driver == thisClientId)
+			return;
+
+		socket.emit('CarServerRefresh',car);
+	});
 
 	socket.on('disconnect',function(){
 		console.log("Client disconnect");
@@ -30,7 +50,6 @@ io.on('connection', function(socket){
 	});
 
 	socket.on('PosRefresh',function(data){
-		console.log('client PosRefresh, id:',thisClientId);
 		data.id = thisClientId;
 		socket.broadcast.emit('PlayerPosRefresh',data);
 	});
@@ -38,5 +57,28 @@ io.on('connection', function(socket){
 	socket.on('Shoot',function(data){
 		console.log(thisClientId + " is Shooting");
 		socket.broadcast.emit('Shooting', data);
+	});
+	
+	socket.on('CarRefresh',function(data){
+		data.driver = thisClientId;
+		Cars.forEach(function(car){
+			if(car.id == data.id)
+				car = data;
+				return;
+		});
+		socket.broadcast.emit('CarServerRefresh', data);
+	});
+
+	socket.on('CloseCar',function(data){
+		data.driver = thisClientId;
+		Cars.forEach(function(car){
+			if(car.id == data.id){
+				console.log("car {0} is closed ({1})", data.id, car.id);
+				car.inUse = "false";
+				socket.broadcast.emit('CarServerRefresh', car);
+				return;
+			}
+		});
+		
 	});
 })
