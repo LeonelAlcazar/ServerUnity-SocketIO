051b837abb7ff5963d38b33348abfa072c994e26
Leonel A. Alcazar <leonelagustinalcazar@gmail.com>
2019-02-01 01:09:42 -0300

    l

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var io = require('socket.io')(process.env.PORT || 3000);
 var shortid = require('shortid');
+
 console.log('Server started');
 
 var Players = [];
@@ -30,10 +31,17 @@ var Ships = [
 	}
 ];
 
+var Items =[];
 
+function broadcastIO(msg,data){
+	io.sockets.emit(msg,data);
+	return;
+}
 
 io.on('connection', function(socket){
 	var thisClientId = shortid.generate();
+	var nick = "";
+	var inventory = "";
 
 	Players.push(thisClientId);
 
@@ -61,8 +69,23 @@ io.on('connection', function(socket){
 		var shipA = {"id":Ships.indexOf(ship),"driver":ship.driver,"inUse":ship.inUse,"x":ship.x,"y":ship.y,"z":ship.z,"rx":ship.rx,"ry":ship.ry,"rz":ship.rz};
 		socket.emit('ShipServerRefresh',shipA);
 	});
+	Items.forEach(function(item){
+		
+		socket.emit('CreateItem',item);
+	});
 
 	socket.on('disconnect',function(){
+		var mysql = require('mysql');
+
+		var client = mysql.createConnection({
+			host: 'db4free.net',
+			user: 'mm0_unity',
+			password: 'mm0_unity',
+			database: 'mm0_unity'
+		});
+		client.query("UPDATE users SET inventory='"+inventory+"' WHERE nickname='"+nick+"'").on('error',function(err){
+			return console.log("[mysql error]",err);
+		});
 		console.log("Client disconnect");
 		Players.splice(Players.indexOf(thisClientId), 1);
 
@@ -91,8 +114,13 @@ io.on('connection', function(socket){
 
 	socket.on('PosRefresh',function(data){
 		data.id = thisClientId;
+		nick = data.nick;
 		socket.broadcast.emit('PlayerPosRefresh',data);
 	});
+
+	socket.on('RefreshInventory',function(data){
+		inventory = data.inventory;
+	})
 
 	socket.on('Shoot',function(data){
 		console.log(thisClientId + " is Shooting");
@@ -151,26 +179,89 @@ io.on('connection', function(socket){
 	});
 
 	socket.on('AddCar',function(data){
-		
-		Cars.push({"driver":"none","inUse":"false","x":String(data.x),"y":String(data.y),"z":String(data.z),"rx":"0","ry":"0","rz":"0"});
-		console.log(Cars[1] + "--");
-		Cars.forEach(function(car){
-			if(car.driver == thisClientId)
-				return;
-			console.log(car);
-			var carA = {"id":Cars.indexOf(car),"driver":car.driver,"inUse":car.inUse,"x":car.x,"y":car.y,"z":car.z,"rx":car.rx,"ry":car.ry,"rz":car.rz};
-			socket.emit('CarServerRefresh',carA);
+
+		var mysql = require('mysql');
+
+		var client = mysql.createConnection({
+			host: 'db4free.net',
+			user: 'mm0_unity',
+			password: 'mm0_unity',
+			database: 'mm0_unity'
+		});
+		console.log("----");
+		client.query("SElECT * FROM users WHERE nickname LIKE '"+nick+"'",function(err,result){
+			if(result[0].money >= data.price){
+				var money = result[0].money - data.price;
+				client.query("UPDATE users SET money='"+money+"' WHERE nickname='"+nick+"'").on('error',function(err){
+					return console.log("[mysql error]",err);
+				});
+				client.end();
+				Cars.push({"driver":"none","inUse":"false","x":String(data.x),"y":String(data.y),"z":String(data.z),"rx":"0","ry":"0","rz":"0"});
+
+				Cars.forEach(function(car){
+					if(car.driver == thisClientId)
+						return;
+					console.log(car);
+					var carA = {"id":Cars.indexOf(car),"driver":car.driver,"inUse":car.inUse,"x":car.x,"y":car.y,"z":car.z,"rx":car.rx,"ry":car.ry,"rz":car.rz};
+					broadcastIO('CarServerRefresh',carA);
+
+				});
+
+				socket.emit('MoneyRefresh',{"money":money});
+
+			}
+		}).on('error',function(err){
+			console.log("[mysql error]",err);
 		});
 	});
 	socket.on('AddShip',function(data){
-		Ships.push({"driver":"none","inUse":"false","x":String(data.x),"y":String(data.y),"z":String(data.z),"rx":"0","ry":"0","rz":"0"});
-		console.log(Ships[Ships.length-1] + "--");
-		Ships.forEach(function(ship){
-			if(ship.driver == thisClientId)
-				return;
-			console.log(ship);
-			var shipA = {"id":Ships.indexOf(ship),"driver":ship.driver,"inUse":ship.inUse,"x":ship.x,"y":ship.y,"z":ship.z,"rx":ship.rx,"ry":ship.ry,"rz":ship.rz};
-			socket.emit('ShipServerRefresh',shipA);
+
+		var mysql = require('mysql');
+
+		var client = mysql.createConnection({
+			host: 'db4free.net',
+			user: 'mm0_unity',
+			password: 'mm0_unity',
+			database: 'mm0_unity'
+		});
+
+		client.query("SElECT * FROM users WHERE nickname LIKE '"+nick+"'",function(err,result){
+			if(result[0].money >= data.price){
+				var money = result[0].money - data.price;
+				console.log(money);
+				client.query("UPDATE users SET money='"+money+"' WHERE nickname LIKE '"+nick+"'").on('error',function(err){
+					return console.log("[mysql error]",err);
+				});
+				client.end();
+
+				Ships.push({"driver":"none","inUse":"false","x":String(data.x),"y":String(data.y),"z":String(data.z),"rx":"0","ry":"0","rz":"0"});
+				console.log(Ships[Ships.length-1] + "--");
+				Ships.forEach(function(ship){
+					if(ship.driver == thisClientId)
+						return;
+					console.log(ship);
+					var shipA = {"id":Ships.indexOf(ship),"driver":ship.driver,"inUse":ship.inUse,"x":ship.x,"y":ship.y,"z":ship.z,"rx":ship.rx,"ry":ship.ry,"rz":ship.rz};
+					broadcastIO('ShipServerRefresh',shipA);
+				});
+
+				socket.emit('MoneyRefresh',{"money":money});
+
+			}
+		}).on('error',function(err){
+			console.log("[mysql error]",err);
+		});
+	});
+	socket.on('AddItem',function(data){
+		console.log('Item');
+		Items.push(data);
+		socket.broadcast.emit('CreateItem',data);
+	});
+	socket.on('DestroyItem',function(data){
+		Items.forEach(function(item){
+			if(Items.indexOf(item) == data.id){
+				Items.splice(Items.indexOf(item),1);
+				socket.broadcast.emit("DestroyItem",data);
+			}
 		});
 	});
 })
